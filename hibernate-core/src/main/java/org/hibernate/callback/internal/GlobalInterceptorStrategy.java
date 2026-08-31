f990c2c35d0dd4280ffb388233fc99a12e1817e9
SeanOkafor <126804794+SeanOkafor@users.noreply.github.com>
2026-08-31 16:54:09 +0100

    HHH-12168 refactored GlobalInterceptorStrategy to use new getBootstrapSafeBean method

--- hibernate-core/src/main/java/org/hibernate/callback/internal/GlobalInterceptorStrategy.java
+++ hibernate-core/src/main/java/org/hibernate/callback/internal/GlobalInterceptorStrategy.java
@@ -7,15 +7,8 @@ package org.hibernate.callback.internal;
 import org.hibernate.Interceptor;
 import org.hibernate.callback.spi.InterceptorStrategy;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.resource.beans.container.internal.CdiBeanContainerDelayedAccessImpl;
-import org.hibernate.resource.beans.container.internal.CdiBeanContainerExtendedAccessImpl;
-import org.hibernate.resource.beans.container.internal.DelayedBeanImpl;
-import org.hibernate.resource.beans.container.internal.JpaCompliantLifecycleStrategy;
-import org.hibernate.resource.beans.container.spi.FallbackContainedBean;
-import org.hibernate.resource.beans.internal.FallbackBeanInstanceProducer;
 import org.hibernate.resource.beans.spi.ManagedBean;
 import org.hibernate.resource.beans.spi.ManagedBeanRegistry;
-
 
 /**
  * @author Sean Okafor
@@ -23,28 +16,10 @@ import org.hibernate.resource.beans.spi.ManagedBeanRegistry;
 public class GlobalInterceptorStrategy implements InterceptorStrategy {
 	private final ManagedBean<? extends Interceptor> interceptorBean;
 
-	//Factory needed to access the service registry
 	public GlobalInterceptorStrategy(Class<? extends Interceptor> interceptorClass,
 									SessionFactoryImplementor sessionFactory) {
 		final var mbr = sessionFactory.getServiceRegistry().getService( ManagedBeanRegistry.class );
-		final var bc = mbr.getBeanContainer();
-
-		if(bc == null) {
-			interceptorBean = new FallbackContainedBean<>(interceptorClass,
-					FallbackBeanInstanceProducer.INSTANCE  );
-		}
-		else if (bc instanceof CdiBeanContainerExtendedAccessImpl ||
-				bc instanceof CdiBeanContainerDelayedAccessImpl ) {
-			interceptorBean = mbr.getBean(interceptorClass);
-		}
-		else {
-			interceptorBean = new DelayedBeanImpl<>( interceptorClass,
-					JpaCompliantLifecycleStrategy.INSTANCE,
-					FallbackBeanInstanceProducer.INSTANCE,
-					bc,
-					false);
-
-		}
+		interceptorBean = mbr.getBootstrapSafeBean( interceptorClass );
 	}
 
 	@Override
